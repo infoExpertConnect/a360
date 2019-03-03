5f78fa217e107c36614ead375e903654a11be8b0
Pa1_jeswani <pa1je5>
2019-03-03 17:53:03 +0530

    trial comit

--- app/src/main/java/com/pawanjes/numbers/view/activity/MainActivity.kt
+++ app/src/main/java/com/pawanjes/numbers/view/activity/MainActivity.kt
@@ -25,6 +25,7 @@ class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelecte
         val toggle = ActionBarDrawerToggle(
             this, drawer_layout, toolbar,
             R.string.navigation_drawer_open,
+
             R.string.navigation_drawer_close
         )
         drawer_layout.addDrawerListener(toggle)
